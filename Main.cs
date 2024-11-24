5a4514dcde4fde7bfd66045d4ad39cdc56049127
Marco Seguri <seguri@users.noreply.github.com>
2024-11-24 16:39:52 +0100

    Set our own name

--- Main.cs
+++ Main.cs
@@ -6,7 +6,7 @@ using Microsoft.PowerToys.Settings.UI.Library;
 using Wox.Plugin;
 using Wox.Plugin.Logger;
 
-namespace Community.PowerToys.Run.Plugin.Demo
+namespace Community.PowerToys.Run.Plugin.JohnnyDecimal
 {
     /// <summary>
     /// Main class of this plugin that implement all used interfaces.
@@ -21,7 +21,7 @@ namespace Community.PowerToys.Run.Plugin.Demo
         /// <summary>
         /// Name of the plugin.
         /// </summary>
-        public string Name => "Demo";
+        public string Name => "JohnnyDecimal";
 
         /// <summary>
         /// Description of the plugin.
