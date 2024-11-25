e85bb5f1c5f6d40ac88a4045f2fb933768662103
Marco Seguri <seguri@users.noreply.github.com>
2024-11-26 00:31:11 +0100

    Delete leftovers from original code

--- Main.cs
+++ Main.cs
@@ -37,14 +37,6 @@ namespace Community.PowerToys.Run.Plugin.JohnnyDecimal
         public IEnumerable<PluginAdditionalOption> AdditionalOptions => [
             new()
             {
-                Key = nameof(CountSpaces),
-                DisplayLabel = "Count spaces",
-                DisplayDescription = "Count spaces as characters",
-                PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Checkbox,
-                Value = CountSpaces,
-            },
-            new()
-            {
                 Key = nameof(RootFolder),
                 DisplayLabel = "Root folder",
                 DisplayDescription = "The path to the Johnny.Decimal folder",
@@ -52,8 +44,6 @@ namespace Community.PowerToys.Run.Plugin.JohnnyDecimal
                 TextValue = RootFolder,
             },
         ];
-
-        private bool CountSpaces { get; set; }
 
         private string? RootFolder { get; set; }
 
@@ -177,8 +167,6 @@ namespace Community.PowerToys.Run.Plugin.JohnnyDecimal
         public void UpdateSettings(PowerLauncherPluginSettings settings)
         {
             Log.Info("UpdateSettings", GetType());
-
-            CountSpaces = settings.AdditionalOptions.SingleOrDefault(x => x.Key == nameof(CountSpaces))?.Value ?? false;
             RootFolder = settings.AdditionalOptions.SingleOrDefault(x => x.Key == nameof(RootFolder))?.TextValue ?? string.Empty;
         }
 
